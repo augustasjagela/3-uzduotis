f962d40eb2827152bc6ce8750158181e94786f4b
augustasjagela <106032669+augustasjagela@users.noreply.github.com>
2022-09-25 12:46:29 +0300

    *added php code

--- counter.php
+++ counter.php
@@ -1,3 +1,19 @@
 <?php 
-$
+$no1 = $_GET["no1"];
+$no2 = $_GET["no2"];
+
+$whichOneIsPressed = $_GET["pressed"];
+
+$result = 0;
+if($whichOneIsPressed == "plus"){
+    $result = $no1 + $no2;
+}else if($whichOneIsPressed == "minus"){
+    $result = $no1 - $no2;
+}else if($whichOneIsPressed == "multiplication"){
+    $result = $no1 * $no2;
+}else if($whichOneIsPressed == "division"){
+    $result = $no1 / $no2;
+}
+
+echo "<div> $result </div>";
 ?>
