07ee3a429d9b1070f21cd82190dd2e9c418fd654
augustasjagela <106032669+augustasjagela@users.noreply.github.com>
2022-09-25 12:33:50 +0300

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="counter.php">
+        <input type="text" name="no1">
+        <input type="text" name="no2">
+        <button type="submit" name="pressed" value="plus">+</button>
+        <button type="submit" name="pressed" value="minus">-</button>
+        <button type="submit" name="pressed" value="multiplication">*</button>
+        <button type="submit" name="pressed" value="division">/</button>
+    </form>
+</body>
+</html>
