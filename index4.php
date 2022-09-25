42c02ef145b674703b1f840f1a64f838275b90ad
augustasjagela <106032669+augustasjagela@users.noreply.github.com>
2022-09-25 18:04:33 +0300

    3.4

---
+++ index4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>3</h1>
+    <form method="POST" action="index4.php">
+        <input type="text" name="name" placeholder="Username">
+        <input type="password" name="pass" placeholder="Password">
+        <button type="submit" name="login">Login</button>
+    </form>
+    <?php
+     
+
+
+     if(isset($_POST["login"])) {
+        $name = $_POST["name"];
+        $password = $_POST["pass"];
+
+        $trueName = "admin";
+        $truePassword == "123456";
+
+        if($name == $trueName && $password == $truePassword) {
+            header("Location: manopaskyra.php");
+        } else {
+            header("Location: 404.php");
+        }
+     }
+        
+     
+
+    ?>
+</body>
+</html>
